64fd70260d35a292d55081a2cd8e06c83dfd297a
Rui Vieira <ruidevieira@googlemail.com>
2020-04-15 14:54:59 +0100

    Remove custom metrics

--- Model.py
+++ Model.py
@@ -16,11 +16,3 @@ class Model(object):
         logger.debug(X)
         _X = pd.DataFrame(X, columns=['income', 'response', 'events'])
         return self.model.predict_proba(_X)
-
-    def metrics(self):
-        return [
-            # a counter which will increase by the given value
-            {"type":"COUNTER","key":"mycounter","value":1}, # a counter which will increase by the given value
-            {"type":"GAUGE","key":"mygauge","value":100}, # a gauge which will be set to given value
-            {"type":"TIMER","key":"mytimer","value":20.2}, # a timer which will add sum and count metrics - assumed millisecs
-            ]
--- README.md
+++ README.md
@@ -2,16 +2,25 @@
 
 ## Build
 
-To build the Seldon image, first train the model using:
+Optionally, you can first train the model using:
 
 ```shell
+$ pip install -r requirements.txt
 $ python train.py
 ```
 
-And then build the image using the `s2i` with:
+This repository contains a pre-trained model, so that you can build the image using the `s2i` as:
 
 ```shell
 $ s2i build https://github.com/ruivieira/ceh-seldon-models \
   seldonio/seldon-core-s2i-python36:0.18 \
   ruivieira/ceh-seldon-models
+```
+
+## Deploy
+
+Deploy on OpenShift with:
+
+```shell
+oc new-app ruivieira/ceh-seldon-models
 ```
